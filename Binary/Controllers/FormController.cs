070354304b9e2b8e5d3c7939a915e98a9217a59b
Rayni Nuñez <timetopwm@gmail.com>
2020-02-28 20:35:23 -0400

    Primer push, Se creo El VIEW principal y el MODEL

---
+++ Binary/Controllers/FormController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Binary.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Resultado()
+        {
+            return View();
+        }
+    }
+}
---
+++ Binary/Models/Nums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Binary.Models
+{
+    public class Nums
+    {
+        public List<int> Numeros { get; set; }
+        public List<int> Binarios { get; set; }
+    }
+}
